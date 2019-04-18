51e615961cf9c35bc3cbd7d7bbb8bbf51f447926
rbboimer <34469424+rbboimer@users.noreply.github.com>
2019-04-17 19:43:38 -0700

    Lambda code for DeleteGoal API

---
+++ functions/DeleteGoal.js
@@ -0,0 +1,45 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: "us-east-1" });
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = (event, context, callback) => {
+  const params = {
+    TableName: process.env.TABLE_NAME, //[ProjectName]-Goals,
+    // 'Key' defines the partition key and sort key of the item to be removed
+    // - 'userId': user identities are federated through the Cognito Identity 
+    //             Pool, we will use the identity id as the user id of the 
+    //             authenticated user
+    // - 'goalId': a unique identifier for the goal (uuid)
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      goalId: event.pathParameters.id
+    }
+  };
+
+  dynamoDb.delete(params, (error, data) => {
+    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials" : true
+    };
+
+    // Return status code 500 on error
+    if (error) {
+      const response = {
+        statusCode: 500,
+        headers: headers,
+        body: error
+      };
+      callback(null, response);
+      return;
+    }
+
+    // Return status code 200 on success
+    const response = {
+      statusCode: 200,
+      headers: headers,
+    };
+    callback(null, response);
+  });
+}
