9ddd39107a576a5ea0cde557b709dab9b465f44d
Maweypeyyu <michaeltrosin@gmx.at>
2019-04-18 14:40:10 -0700

    Removes hard coded region for delete lambda (#2)

--- functions/DeleteGoal.js
+++ functions/DeleteGoal.js
@@ -1,6 +1,5 @@
 const AWS = require('aws-sdk');
 
-AWS.config.update({ region: "us-east-1" });
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event, context, callback) => {
